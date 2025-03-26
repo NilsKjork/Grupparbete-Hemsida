c887b852ce05744a636f758b74ce4e49976a92c0
wezams <148435240+wezams@users.noreply.github.com>
2025-03-26 23:09:47 +0100

    buyed

--- jjavascript/favoritVaror.js
+++ jjavascript/favoritVaror.js
@@ -1,3 +1,12 @@
 import { draw } from "./SparadeVaror";
 import { sparadeVaror } from "./SparadeVaror";
 draw(sparadeVaror);
+
+let buyButton = null;
+
+document.addEventListener('DOMContentLoaded', function () {
+    buyButton = document.getElementById('buy_button');
+    buyButton.addEventListener('click', function(){
+        alert("Du köpte" + JSON.stringify(sparadeVaror));
+    });
+});
