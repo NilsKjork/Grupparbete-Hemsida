2377b26c432f0d7b8bfa13e318d20063e8383a5b
wezams <148435240+wezams@users.noreply.github.com>
2025-03-30 00:31:32 +0100

    now works

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import open from 'open';
-import { dirname} from 'path';
+import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
- 
-const filePath = (__dirname, 'index.html');
+
+const filePath = __dirname + '/index.html';
+
 await open(`file://${filePath}`);
- 
+
 console.log('Opening index.html...');
